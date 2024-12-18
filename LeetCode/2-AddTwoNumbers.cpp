33c62ba21f4b77b77d8b800d61b5302b33c615ae
Dhanush kumar <130242288+Dhanush-kumar-dk@users.noreply.github.com>
2024-12-18 09:13:23 +0530

    Update 2-AddTwoNumbers.cpp

--- LeetCode/2-AddTwoNumbers.cpp
+++ LeetCode/2-AddTwoNumbers.cpp
@@ -1,1 +1,33 @@
-
+#Javascript
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    if (l1 == null && l2 == null) {
+        return null
+    }
+    let value = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+    let carry = null;
+    if (l1 != null && l1.next != null) {
+        carry = l1.next;
+    }
+    if (value >= 10) {
+        value -= 10;
+        if (carry != null) {
+            carry.val += 1;
+        } else {
+            carry = new ListNode(1, null);
+        }
+    }
+    let output = new ListNode(value, addTwoNumbers(carry, l2 == null ? null : l2.next));
+    return output;
+}
